e0afd82780bd4aff01ebb6aa191a05c361702ff7
LopezTheodore <135138129+LopezTheodore@users.noreply.github.com>
2024-04-04 12:30:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Agnes Theodore",
+  title: "Web Designer & Developer",
+  email: "agnes@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
